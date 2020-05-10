a25c68bc8d8aa449d14cb7eabacbc3ba7379074f
Svetoslav <svetoslavangelovkolev@gmail.com>
2020-05-10 15:46:24 +0300

    Updated Custom List

--- CustomList/CustomListExercise/StartUp.cs
+++ CustomList/CustomListExercise/StartUp.cs
@@ -23,6 +23,7 @@ namespace CustomListExercise
             }
             Console.WriteLine("capacity: "+myList.capacity);
             Console.WriteLine("count: " + myList.Count);
+            Console.WriteLine("removing element '30'");
             myList.Remove(30);
             Console.WriteLine("capacity: " + myList.capacity);
             Console.WriteLine("count: " + myList.Count);
@@ -31,6 +32,7 @@ namespace CustomListExercise
                 Console.WriteLine(item);
             }
             myList.Remove(40);
+            Console.WriteLine("removing element '40'");
             Console.WriteLine("capacity: " + myList.capacity);
             Console.WriteLine("count: " + myList.Count);
             foreach (var item in myList)
@@ -38,10 +40,12 @@ namespace CustomListExercise
                 Console.WriteLine(item);
             }
             myList.Swap(0, 3);
+            Console.WriteLine("Swapping elements at index 0 and 3");
             foreach (var item in myList)
             {
                 Console.WriteLine(item);
             }
+            Console.WriteLine("Inserting elements 3 ,4 ,5 ,6 ,7 all at index 1");
             myList.Insert(3, 1);
             myList.Insert(4, 1);
             myList.Insert(5, 1);
@@ -53,6 +57,7 @@ namespace CustomListExercise
             }
             Console.WriteLine("capacity: " + myList.capacity);
             Console.WriteLine("count: " + myList.Count);
+            Console.WriteLine("removing elements 3 ,4 ,5 ,6");
             myList.Remove(3);
             myList.Remove(4);
             myList.Remove(5);
@@ -63,16 +68,36 @@ namespace CustomListExercise
             {
                 Console.WriteLine(item);
             }
+            Console.WriteLine("print index of element '3'");
             Console.WriteLine(myList.IndexOf(3));
+            Console.WriteLine("check if list contains element '3'");
             Console.WriteLine(myList.Contains(3));
+            Console.WriteLine("check if list contains element '4'");
             Console.WriteLine(myList.Contains(4));
+            Console.WriteLine("print index of element '5' , if -1 = list does not contain 5");
             Console.WriteLine(myList.IndexOf(5));
+            Console.WriteLine("swap index 4 with index 0");
             myList.Swap(4,0);
+            Console.WriteLine("swap index 2 with index 1");
             myList.Swap(2, 1);
             foreach (var item in myList)
             {
                 Console.WriteLine(item);
             }
+            Console.WriteLine("insert element 3 at index 0");
+            myList.Insert(3, 0);
+            foreach (var item in myList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("printing elements at index 3 and 1 with indexing");
+            Console.WriteLine(myList[3]);
+            Console.WriteLine(myList[1]);
+            Console.WriteLine("print number of elements bigger than 15");
+            Console.WriteLine(myList.CountGreaterThan(15));
+            Console.WriteLine("print number of elements bigger than 5");
+            Console.WriteLine(myList.CountGreaterThan(5));
+            Console.WriteLine("Clear List");
             myList.Clear();
             Console.WriteLine("capacity: " + myList.capacity);
             Console.WriteLine("count: " + myList.Count);
