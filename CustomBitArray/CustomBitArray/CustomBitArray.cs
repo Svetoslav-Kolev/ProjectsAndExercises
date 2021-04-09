169c6c4240e6418bd292105890389b96441bcd18
Svetaka <svetoslavangelovkolev@gmail.com>
2021-04-09 14:56:17 +0300

    CustomBitArray Addition

---
+++ CustomBitArray/CustomBitArray/CustomBitArray.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomBitArray
+{
+    class CustomBitArray 
+    {
+        private int[] bitArray; //actual array that holds the bits (32 bits per element)
+        private int count;
+        public int Count 
+        {
+            get
+            {
+                return count;
+            }
+            set
+            {
+                count = value;
+            }
+        }
+        public CustomBitArray(int n)
+        {
+            count = 0;
+            bitArray = new int[(n>>5)+1];   //effectively divides by 2^5 (32)
+        }
+
+        
+        public void SetBitToOne(int position) //position is acting as the integer you want to store
+        {
+            // Find index of array that holds this bit 
+            int index = (position / 32);
+            //Find which position the bit takes in array[index]
+            int bitNumber = (position % 32);
+            //(1<<bitNumber) sets all bits to 0 except the one on the correct position
+            //the | operator sets the bit at this position to One while not interfering with the other bits since (1<<bitNumber) has only one positive bit
+            bitArray[index] |= (1 << bitNumber);
+
+            count++;
+        }
+        public  bool GetBit(int position)
+        {
+            int index = (position / 32);
+            int bitNumber = (position %32 );
+            //returns false if both integers are not 1
+            return (bitArray[index] & (1 << bitNumber)) != 0;
+        }
+        public void SetBitToZero(int position)
+        {
+            int index = (position /32 );
+            int bitNumber = (position % 32);
+            //reverses (1<<bitNumber) so that it has only one zero
+            //it doesn't interfere with the other bits, it can only change the bit at the given position (since all other bits are 1)
+            bitArray[index] &= ~(1 << bitNumber);
+            count--;
+        }
+    } 
+}
---
+++ CustomBitArray/CustomBitArray/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+
+namespace CustomBitArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //FIllFile();
+            //CheckDuplicate();
+
+
+            CustomBitArray bitArray = new CustomBitArray(270);
+            bitArray.SetBitToOne(0);
+      
+            bitArray.SetBitToOne(270);
+            var resultMin = bitArray.GetBit(0);
+            var resultMax = bitArray.GetBit(271);
+           
+            Console.WriteLine();
+        }
+        public static void FIllFile()
+        {
+            string binaryFileName = "NumbersSmall.bin";
+
+            using (FileStream fileStream = new FileStream(binaryFileName, FileMode.Create))
+            {
+                using (BinaryWriter binaryWriter = new BinaryWriter(fileStream))
+                {
+                    for (int i = 0; i < 1000000; i++)
+                    {
+                        binaryWriter.Write(i);
+                    }
+               
+                    //binaryWriter.Write(99999);
+                    binaryWriter.Write(1);
+                }
+            }
+        }
+        public static void CheckDuplicate()
+        {
+            CustomBitArray bitArray = new CustomBitArray(int.MaxValue); // file currently holds only integers
+            string binaryFileName = "NumbersSmall.bin";
+            long toRead = new FileInfo(binaryFileName).Length;
+            using (FileStream fileStream = new FileStream(binaryFileName, FileMode.Open))
+            {
+                using (BinaryReader binaryReader = new BinaryReader(fileStream))
+                {
+                    for (int i = 0; i < toRead / 4; i++)
+                    {
+                        var result = binaryReader.ReadInt32();
+                        if (bitArray.GetBit(result) == true)
+                        {
+                            Console.WriteLine(result);
+                            break;
+                        }
+                        bitArray.SetBitToOne(result);
+                    }
+
+
+                }
+            }
+        }
+    }
+}
